11e1763f78680812877237c728cb91c26376c234
Jacob West <gduden64@gmail.com>
2019-07-01 13:39:22 -0700

    Adding All Files

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8" />
+        <title> Starting With Android</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+        	<h1> Android Development</h1>
+        </header>
+        
+        <nav>
+        	<a id="current" href="index.php">Home</a>
+        	<a href="start.html">Start</a>
+        	<a href="development.html">Development</a>
+        	<a href="finish.html">Finish</a>
+        	
+        </nav>
+        <hr />
+        
+        <br /><br />
+        
+        <main> 
+        	<figure>
+        		<img src="img/code_image.png" alt = "Picture" />
+        	</figure>
+        
+        <div id="textbox">
+        	<b>Android Development</b> <br />
+        	<p>Android development is one of the few ways someone could get started <br />
+        	in mobile development completely for free!</p>
+        	
+        	<p>On this webpage, you can learn all about starting and pushing out a project of your own.</p>
+        
+        <em>"With ordinary talent and extraordinary 
+        <strong>preserverance</strong>, all things are attainable."</em><br />
+        - Thomas F. Buxton
+        </div>
+        
+        </main>
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+        	
+            <hr>
+            <img src="img/logo.png" alt = "CSUMB Logo" /> <br />
+            CST 336 Internet Programming. 2019&copy; West <br />
+            <strong>Disclaimer :</strong> The information 
+            in this webpage is fictitious.<br />
+            It is used for academic purposes only.
+            
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
